73a48d44a855f156977babf9625ce313513f1986
Timon Wong <timon86.wang@gmail.com>
2017-10-25 13:35:11 +0800

    Add script to build manylinux

---
+++ scripts/build-wheels.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -i http://pypi.doubanio.com/simple cython
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
