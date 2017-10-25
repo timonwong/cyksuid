5b8013b2be2a068f990a26d7f4545acd1ad26998
Timon Wong <timon86.wang@gmail.com>
2017-10-25 13:59:12 +0800

    Fix manylinux build script

--- manylinux.sh
+++ manylinux.sh
@@ -2,4 +2,4 @@
 set -e -x
 
 docker run --rm -v $(PWD):/io quay.io/pypa/manylinux1_x86_64 /io/scripts/build-wheels.sh
-#docker run --rm -v $(PWD):/io quay.io/pypa/manylinux1_i686 linux32 /io/scripts/build-wheels.sh
+docker run --rm -v $(PWD):/io quay.io/pypa/manylinux1_i686 linux32 /io/scripts/build-wheels.sh
