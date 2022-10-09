52b26b564756295b02459c10f170a50e57837b47
Timon Wong <timon86.wang@gmail.com>
2022-10-09 16:09:35 +0800

    feat: add type hint
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- cyksuid/fast_base62.pyx
+++ cyksuid/fast_base62.pyx
@@ -1,5 +1,9 @@
 from cpython.bytes cimport PyBytes_FromStringAndSize
 
+DEF ERR_B62_INSUFFICIENT_OUTPUT_BUFFER = -1
+DEF ERR_B62_INSUFFICIENT_INPUT_BUFFER = -2
+DEF ERR_B62_INVALID_INPUT = -3
+
 DEF _BASE62_BYTE_LENGTH = 20
 DEF _BASE62_ENCODED_LENGTH = 27
 
@@ -12,16 +16,15 @@ cpdef bytes fast_b62encode(bytes src):
     err_code = ksuid_b62_encode(dst_buf, _BASE62_ENCODED_LENGTH, src_buf, len(src))
 
     # Checking error codes
-    if err_code == 0:
-        pass
-    elif err_code == -1:
-        raise ValueError("Insufficient output buffer size")
-    elif err_code == -2:
-        raise ValueError("Insufficient input buffer size")
-    elif err_code == -3:
-        raise ValueError("Invalid input buffer")
-    else:
-        raise ValueError("Unknown error: %d" % err_code)
+    if err_code != 0:
+        if err_code == ERR_B62_INSUFFICIENT_OUTPUT_BUFFER:
+            raise ValueError("Insufficient output buffer size")
+        elif err_code == ERR_B62_INSUFFICIENT_INPUT_BUFFER:
+            raise ValueError("Insufficient input buffer size")
+        elif err_code == ERR_B62_INVALID_INPUT:
+            raise ValueError("Invalid input buffer")
+        else:
+            raise ValueError("Unknown error: %d" % err_code)
 
     return PyBytes_FromStringAndSize(dst_buf, _BASE62_ENCODED_LENGTH)
 
@@ -34,15 +37,14 @@ cpdef bytes fast_b62decode(bytes src):
     err_code = ksuid_b62_decode(dst_buf, _BASE62_BYTE_LENGTH, src_buf, len(src))
 
     # Checking error codes
-    if err_code == 0:
-        pass
-    elif err_code == -1:
-        raise ValueError("Insufficient output buffer size")
-    elif err_code == -2:
-        raise ValueError("Insufficient input buffer size")
-    elif err_code == -3:
-        raise ValueError("Invalid input buffer")
-    else:
-        raise ValueError("Unknown error: %d" % err_code)
+    if err_code != 0:
+        if err_code == ERR_B62_INSUFFICIENT_OUTPUT_BUFFER:
+            raise ValueError("Insufficient output buffer size")
+        elif err_code == ERR_B62_INSUFFICIENT_INPUT_BUFFER:
+            raise ValueError("Insufficient input buffer size")
+        elif err_code == ERR_B62_INVALID_INPUT:
+            raise ValueError("Invalid input buffer")
+        else:
+            raise ValueError("Unknown error: %d" % err_code)
 
     return PyBytes_FromStringAndSize(<char *>dst_buf, _BASE62_BYTE_LENGTH)
