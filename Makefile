68b8883d122d7966e643b1f3a6e50d7e5758be2d
Timon Wong <timon86.wang@gmail.com>
2022-10-11 12:33:19 +0800

    build(ci): Add coverage reporting (#23)
    * add coverage test
    
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>
    
    * add test ci
    
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>
    
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,23 @@
+.PHONY: build-debug
+build-debug:
+	export CY_DEBUG=1; python setup.py build_ext --inplace --with-cython
+
+.PHOHY: build
+build:
+	unset CY_DEBUG; python setup.py build_ext --inplace --with-cython
+
+.PHONY: coverage
+coverage: build-debug
+	PYTHONPATH=. pytest -v --cov --cov-report=term --cov-report=xml:coverage.xml
+	@for f in $(shell find cyksuid -type f -name "*.pyx"); do \
+		echo "Annotating $${f}"; \
+		cython -3 --annotate-coverage=coverage.xml $${f}; \
+	done
+
+.PHONY: test
+test: build
+	PYTHONPATH=. pytest -v
+
 .PHONY: bench
-bench:
+bench: build
 	pytest bench.py --benchmark-json bench.json && jq '.benchmarks[] | {name, "mean": .stats["mean"]} ' bench.json
--- cyksuid/fast_base62.pyx
+++ cyksuid/fast_base62.pyx
@@ -1,18 +1,11 @@
 from cpython.bytes cimport PyBytes_FromStringAndSize
 
-DEF ERR_B62_INSUFFICIENT_OUTPUT_BUFFER = -1
-DEF ERR_B62_INSUFFICIENT_INPUT_BUFFER = -2
-DEF ERR_B62_INVALID_INPUT = -3
-
-DEF _BASE62_BYTE_LENGTH = 20
-DEF _BASE62_ENCODED_LENGTH = 27
-
 
 cdef bytes _fast_b62encode(const uint8_t* src, size_t src_len):
-    cdef char[_BASE62_ENCODED_LENGTH] dst_buf
+    cdef char[BASE62_ENCODED_LENGTH] dst_buf
     cdef int err_code
 
-    err_code = ksuid_b62_encode(dst_buf, _BASE62_ENCODED_LENGTH, src, src_len)
+    err_code = ksuid_b62_encode(dst_buf, BASE62_ENCODED_LENGTH, src, src_len)
 
     # Checking error codes
     if err_code != 0:
@@ -23,16 +16,16 @@ cdef bytes _fast_b62encode(const uint8_t* src, size_t src_len):
         elif err_code == ERR_B62_INVALID_INPUT:
             raise ValueError("Invalid input buffer")
         else:
-            raise ValueError("Unknown error: %d" % err_code)
+            raise ValueError("Unknown error: %d" % err_code)  # pragma: no cover
 
-    return PyBytes_FromStringAndSize(dst_buf, _BASE62_ENCODED_LENGTH)
+    return PyBytes_FromStringAndSize(dst_buf, BASE62_ENCODED_LENGTH)
 
 
 cdef bytes _fast_b62decode(const char* src, size_t src_len):
-    cdef unsigned char[_BASE62_BYTE_LENGTH] dst_buf
+    cdef unsigned char[BASE62_BYTE_LENGTH] dst_buf
     cdef int err_code
 
-    err_code = ksuid_b62_decode(dst_buf, _BASE62_BYTE_LENGTH, src, src_len)
+    err_code = ksuid_b62_decode(dst_buf, BASE62_BYTE_LENGTH, src, src_len)
 
     # Checking error codes
     if err_code != 0:
@@ -43,9 +36,9 @@ cdef bytes _fast_b62decode(const char* src, size_t src_len):
         elif err_code == ERR_B62_INVALID_INPUT:
             raise ValueError("Invalid input buffer")
         else:
-            raise ValueError("Unknown error: %d" % err_code)
+            raise ValueError("Unknown error: %d" % err_code)  # pragma: no cover
 
-    return PyBytes_FromStringAndSize(<char *>dst_buf, _BASE62_BYTE_LENGTH)
+    return PyBytes_FromStringAndSize(<char *>dst_buf, BASE62_BYTE_LENGTH)
 
 
 def fast_b62encode(bytes src):
