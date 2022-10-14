b7a2cde98169548a53663a0b1c3c9abe3b5774e7
Timon Wong <timon86.wang@gmail.com>
2022-10-14 12:02:41 +0800

    fix: fix build on arm, add optimizer args (#27)
    fix build on arm, add optimization args

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 import os
 import os.path
 import re
+import subprocess
 import sys
 import sysconfig
+import warnings
 from distutils import util
-from typing import Any, Dict, List, Tuple
+from distutils.command import build_ext
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
 
 import pkg_resources
 from setuptools import Extension, setup
 
-IS_DEBUG = os.getenv("CYKSUID_DEBUG", "")
+IS_DEBUG = os.getenv("CYKSUID_DEBUG", "") == "1"
 
 ext_macros: List[Tuple[str, str]] = []
-ext_cythonize_kwargs: Dict[str, Any] = {
-    "force": True,
-    "compiler_directives": {},
-}
 ext_include_dirs = []
 
-if IS_DEBUG == "1":
+if IS_DEBUG:
     print("Build in debug mode")
     # Adding cython line trace for coverage report
     ext_macros += ("CYTHON_TRACE_NOGIL", "1"), ("CYTHON_TRACE", "1")
@@ -26,17 +25,6 @@ if IS_DEBUG == "1":
     # tests inside the cython package
     ext_include_dirs += [".."]
     # Some extra info for cython compiler
-    ext_cythonize_kwargs.update(
-        {
-            "gdb_debug": True,
-            "annotate": True,
-            "compiler_directives": {
-                "linetrace": True,
-                "profile": True,
-                "binding": True,
-            },
-        }
-    )
 
 
 try:
@@ -75,38 +63,6 @@ if USE_CYTHON:
 else:
     suffix = ".cpp"
 
-extra_compile_args: List[str] = []
-if sys.platform != "win32":
-    extra_compile_args.append("-Wno-write-strings")
-    extra_compile_args.append("-Wno-invalid-offsetof")
-    extra_compile_args.append("-Wno-sign-compare")
-    extra_compile_args.append("-Wno-unused-variable")
-    extra_compile_args.append("-std=c++14")
-
-if sys.platform == "darwin":
-    extra_compile_args.append("-Wno-shorten-64-to-32")
-    extra_compile_args.append("-Wno-deprecated-register")
-
-    # https://developer.apple.com/documentation/xcode_release_notes/xcode_10_release_notes
-    # C++ projects must now migrate to libc++ and are recommended to set a
-    # deployment target of macOS 10.9 or later, or iOS 7 or later.
-    if sys.platform == "darwin":
-        mac_target = str(sysconfig.get_config_var("MACOSX_DEPLOYMENT_TARGET"))
-        if mac_target and (
-            pkg_resources.parse_version(mac_target)
-            < pkg_resources.parse_version("10.9.0")
-        ):
-            os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
-            os.environ["_PYTHON_HOST_PLATFORM"] = re.sub(
-                r"macosx-[0-9]+\.[0-9]+-(.+)", r"macosx-10.9-\1", util.get_platform()
-            )
-
-# MSVS default is dymanic
-if sys.platform == "win32":
-    extra_compile_args.append("/MT")
-
-if "clang" in os.popen("$CC --version 2> /dev/null").read():
-    extra_compile_args.append("-Wno-shorten-64-to-32")
 
 ext_modules = [
     Extension(
@@ -114,7 +70,6 @@ ext_modules = [
         sources=["cyksuid/fast_base62" + suffix, "cyksuid/cbase62.cc"],
         define_macros=ext_macros,
         include_dirs=ext_include_dirs,
-        extra_compile_args=extra_compile_args,
         language="c++",
     ),
     Extension(
@@ -122,21 +77,193 @@ ext_modules = [
         sources=["cyksuid/_ksuid" + suffix],
         define_macros=ext_macros,
         include_dirs=ext_include_dirs,
-        extra_compile_args=extra_compile_args,
         language="c++",
     ),
 ]
 
 
-if USE_CYTHON:
-    ext_cythonize_kwargs["compiler_directives"].update(
-        {
-            "language_level": "3",
-            "embedsignature": True,
-            "binding": True,
-        }
-    )
-    ext_modules = cythonize(ext_modules, **ext_cythonize_kwargs)
+class BuildExt(build_ext.build_ext):
+    def initialize_options(self) -> None:
+        super().initialize_options()
+        self.cython_kwargs: Optional[Dict[str, Any]] = None
+        self.cython_compiler_directives: Optional[dict] = None
+        self.extra_compile_args: List[str] = []
+        self.extra_link_args: List[str] = []
+
+    def finalize_options(self) -> None:
+        super().finalize_options()
+        if self.cython_kwargs is None:
+            self.cython_kwargs = {
+                "force": True,
+            }
+        if self.cython_compiler_directives is None:
+            self.cython_compiler_directives = {
+                "language_level": "3",
+                "embedsignature": True,
+                "binding": True,
+            }
+
+        if IS_DEBUG:
+            self.cython_kwargs.update(
+                {
+                    "gdb_debug": True,
+                    "annotate": True,
+                }
+            )
+            self.cython_compiler_directives.update(
+                {
+                    "linetrace": True,
+                    "profile": True,
+                    "binding": True,
+                }
+            )
+
+    def build_extensions(self) -> None:
+        is_msvc = self.compiler.compiler_type == "msvc"
+        is_clang = hasattr(self.compiler, "compiler_cxx") and (  # noqa: F841
+            "clang++" in self.compiler.compiler_cxx
+        )
+        is_windows = sys.platform[:3] == "win"
+        is_mingw = is_windows and (
+            self.compiler.compiler_type.lower()
+            in ["mingw32", "mingw64", "mingw", "msys", "msys2", "gcc", "g++"]
+        )
+
+        check_args: List[str] = []
+        if sys.platform == "darwin":
+            check_args += [
+                "-Wno-deprecated-register",
+            ]
+
+            # https://developer.apple.com/documentation/xcode_release_notes/xcode_10_release_notes
+            # C++ projects must now migrate to libc++ and are recommended to set a
+            # deployment target of macOS 10.9 or later, or iOS 7 or later.
+            mac_target = str(sysconfig.get_config_var("MACOSX_DEPLOYMENT_TARGET"))
+            if mac_target and (
+                pkg_resources.parse_version(mac_target)
+                < pkg_resources.parse_version("10.9.0")
+            ):
+                os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+                os.environ["_PYTHON_HOST_PLATFORM"] = re.sub(
+                    r"macosx-[0-9]+\.[0-9]+-(.+)",
+                    r"macosx-10.9-\1",
+                    util.get_platform(),
+                )
+
+        if is_msvc:
+            self.extra_compile_args += [
+                "/MT",
+                "/O2",
+                "/std:c++14",
+            ]
+        else:
+            check_args += [
+                "-Wno-write-strings",
+                "-Wno-invalid-offsetof",
+                "-Wno-sign-compare",
+                "-Wno-unused-variable",
+                "-Wno-shorten-64-to-32",
+            ]
+            self.add_extra_compile_args(check_args)
+
+        if not is_mingw:
+            self.add_highest_supported_cxx_standard()
+
+        if not IS_DEBUG:
+            self.add_O3()
+            self.add_no_math_errno()
+            self.add_no_trapping_math()
+            if not is_windows:
+                self.add_link_time_optimization()
+
+        for e in self.extensions:
+            e.extra_compile_args += self.extra_compile_args
+
+        if USE_CYTHON:
+            self.extensions = cythonize(
+                self.extensions,
+                compiler_directives=self.cython_compiler_directives,
+                **cast(dict, self.cython_kwargs),
+            )
+        super().build_extensions()
+
+    def add_extra_compile_args(self, args: List[str]) -> None:
+        for arg in args:
+            if self.test_supports_compile_arg(arg):
+                self.extra_compile_args.append(arg)
+
+    def add_highest_supported_cxx_standard(self):
+        cxx17 = "-std=c++17"
+        cxx14 = "-std=gnu++14"
+        cxx11 = "-std=c++11"
+        if self.test_supports_compile_arg(cxx17):
+            self.extra_compile_args.append(cxx17)
+        elif self.test_supports_compile_arg(cxx14):
+            self.extra_compile_args.append(cxx14)
+        elif self.test_supports_compile_arg(cxx11):
+            self.extra_compile_args.append(cxx11)
+        else:
+            msg = "\n\n\nWarning: compiler does not support C++11, compilation of cyksuid might fail without it.\n\n\n"
+            warnings.warn(msg)
+
+    def add_O3(self):
+        O3 = "-O3"
+        if self.test_supports_compile_arg(O3):
+            self.extra_compile_args.append(O3)
+
+    def add_no_math_errno(self):
+        arg_fnme = "-fno-math-errno"
+        if self.test_supports_compile_arg(arg_fnme):
+            self.extra_compile_args.append(arg_fnme)
+            self.extra_link_args.append(arg_fnme)
+
+    def add_no_trapping_math(self):
+        arg_fntm = "-fno-trapping-math"
+        if self.test_supports_compile_arg(arg_fntm):
+            self.extra_compile_args.append(arg_fntm)
+            self.extra_link_args.append(arg_fntm)
+
+    def add_link_time_optimization(self):
+        arg_lto = "-flto"
+        if self.test_supports_compile_arg(arg_lto):
+            self.extra_compile_args.append(arg_lto)
+            self.extra_link_args.append(arg_lto)
+
+    def test_supports_compile_arg(self, comm: Union[str, List[str]]) -> bool:
+        if not hasattr(self.compiler, "compiler_cxx"):
+            return False
+        if not isinstance(comm, list):
+            comm = [comm]
+
+        is_supported = False
+
+        try:
+            if not isinstance(self.compiler.compiler_cxx, list):
+                cmd = list(self.compiler.compiler_cxx)
+            else:
+                cmd = self.compiler.compiler_cxx
+        except Exception as e:
+            print("Error: could not get compiler_cxx: %s" % e)
+            cmd = self.compiler.compiler_cxx
+
+        print("--- Checking compiler support for option '%s'" % " ".join(comm), end="")
+        fname = "cyksuid_compiler_testing.cpp"
+        try:
+            with open(fname, "w") as ftest:
+                ftest.write("int main(int argc, char**argv) {return 0;}\n")
+
+            val_good = subprocess.call(cmd + [fname])
+            val = subprocess.call(cmd + comm + [fname])
+            is_supported = val == val_good
+        except Exception:
+            is_supported = False
+        finally:
+            try:
+                os.remove(fname)
+            except Exception:
+                pass
+        print(" ... %s" % ("yes" if is_supported else "no"))
+        return is_supported
 
 
 setup(
@@ -144,6 +271,7 @@ setup(
     version="2.0.0",
     description="Cython implementation of ksuid",
     ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExt},
     long_description=open("README.md").read() if os.path.exists("README.md") else "",
     long_description_content_type="text/markdown",
     url="https://github.com/timonwong/cyksuid",
@@ -154,7 +282,7 @@ setup(
     package_data={
         "cyksuid": ["*.pyx", "*.pxd", "*.pyi", "py.typed", "*.h", "*.cc", "*.cpp"],
     },
-    keywords="ksuid",
+    keywords=["ksuid"],
     python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
